7d7f274bcbaab6f06f6f7d4b2552f184a227739e
tudda <tentufancr7@gmail.com>
2022-11-04 01:37:16 +0700

    fix_bug_my_group

--- src/java/controller_group/MyGroup.java
+++ src/java/controller_group/MyGroup.java
@@ -7,7 +7,7 @@ package controller_group;
 import controller.Utils;
 import dao.AccountGroupDAO;
 import dao.GroupTaskDAO;
-import java.io.IOException; 
+import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,21 +21,34 @@ import model.GroupTask;
  * @author 84352
  */
 public class MyGroup extends HttpServlet {
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-             HttpSession httpSession = request.getSession(); 
+            HttpSession httpSession = request.getSession();
             GroupTaskDAO groupTaskDAO = new GroupTaskDAO();
             ArrayList<GroupTask> list = groupTaskDAO.getAllGroupTaskByUserName(Utils.getAccountLogin(request).getUsername());
-            
+
             AccountGroupDAO accountGroupDAO = new AccountGroupDAO();
+            int checkmygr = 0;
             for (int i = 0; i < list.size(); i++) {
                 String la[] = accountGroupDAO.getAccGr(list.get(i).getGroupID()).split(" ");
-                 list.get(i).setUsernames(la);
+                for (int j = 0; j < la.length; j++) {
+                    String string = la[j].trim();
+                    if (string.equals(Utils.getAccountLogin(request).getUsername())) {
+                        checkmygr = 1;
+                        break;
+                    }
+                }
+                if (checkmygr == 0) {
+                    list.remove(i);
+                } else {
+                    list.get(i).setUsernames(la);
+                }
+                checkmygr = 0;
             }
-            request.setAttribute("list", list); 
+            request.setAttribute("list", list);
             if (groupTaskDAO.getAllGroupTaskByUserName(Utils.getAccountLogin(request).getUsername()).isEmpty()) {
                 request.setAttribute("mess", "You do not have any group");
             } else {
